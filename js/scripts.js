0e16bf2cd27fd84f2a8458d674b6572f5ea36691
&rew Peterson <drewcpete@gmail.com>
2019-08-23 14:44:49 -0700

    adds newbranch

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,27 @@ var outArray = []
 
 function compTalk(origNum) {
 
-// debugger;
-  for (var i = 0; i <= origNum; i++) {
-    if (i == 3) {
+// before using the includes method, I tried setting i == "1"
+  for (var i = 0; i <= origNum; i+= 1) {
+    console.log(typeof(i));
+    if ([i] == "3") {
+      console.log(i);
       outArray.push("I'm sorry, Ben or Elly. I'm afraid I can't do that.");
-    } else if (i == 2) {
+      console.log(outArray);
+      return true;
+    } else if ([i] == "2") {
+      console.log(i);
       outArray.push("Boop!");
-    } else if (i == 1) {
+      console.log(outArray);
+      return true;
+    } else if ([i] == "1") {
+      console.log(i);
       outArray.push("Beep!");
+      console.log(outArray);
+      return true;
+    }
+    else {
+      outArray.push(i)
     }
   }
   return outArray;
@@ -22,12 +35,13 @@ function compTalk(origNum) {
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
-    var origNum = $("input#numberInput").val().split("");
-
+    var origNum = $("input#numberInput").val().split([]);
     console.log(origNum);
+
     var finished = compTalk(origNum);
     console.log(finished);
-    $("p#textOutput").text("finished" + finished);
+    finished = finished.join(", " + "")
+    $("p#textOutput").text(finished);
 
 
   })
