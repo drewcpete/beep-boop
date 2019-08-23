2e10dc5deca2ca16b4c388a23f4e95bf2f8ab372
&rew Peterson <drewcpete@gmail.com>
2019-08-23 10:06:53 -0700

    attempting to debug js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
-// Business Logic
-var origNum = $("input#numberInput").val();
-
-var numArray = origNum.split("");
-
-var output = ""
 // Front end logic
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
+    var origNum = $("input#numberInput").val();
+        debugger;
+    console.log(origNum);
+
+    var numArray = [];
+    numArray = origNum;
+    numArray = numArray.split("");
+    var output = [];
+
     for (var i = 0; i <= numArray.length; i++) {
       if (numArray[i] == 1) {
-        output.text("Beep!")
+        output.append("Beep!")
       } else if (numArray[i] == 2) {
-        output.text("Boop!")
+        output.append("Boop!")
       } else if (numArray[i] == 3) {
-        output.text("I'm sorry, Ben or Elly. I'm afraid I can't do that.")
+        output.append("I'm sorry, Ben or Elly. I'm afraid I can't do that.")
       }
     }
+    $("p#textOutput").text(output);
+
 
   })
 })
+
+
+// Business Logic
