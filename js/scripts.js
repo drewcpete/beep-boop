ab3fa8b7a74536bb837619e0c309b8fbd611f9e5
&rew Peterson <drewcpete@gmail.com>
2019-08-23 09:38:03 -0700

    adds for loop changing 1,2and3 to sentence strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
+// Business Logic
+var origNum = $("input#numberInput").val();
+
+var numArray = origNum.split("");
+
+var output = ""
+// Front end logic
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
-    var origNum = $("input#numberInput").val();
-
-    var numArray = origNum.split("");
+    for (var i = 0; i <= numArray.length; i++) {
+      if (numArray[i] == 1) {
+        output.text("Beep!")
+      } else if (numArray[i] == 2) {
+        output.text("Boop!")
+      } else if (numArray[i] == 3) {
+        output.text("I'm sorry, Ben or Elly. I'm afraid I can't do that.")
+      }
+    }
 
   })
 })
